484c414a1247c487c7559f37672c00a2cbe26a78
RalphGerrard <71027652+RalphGerrard@users.noreply.github.com>
2021-03-16 18:46:56 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,39 +1,24 @@
-var car,wall;
-var speed,weight;
-var deformation;
-
+var fixedR, movingR;
 
 function setup() {
   createCanvas(1600,400);
-  speed=random(55,90);
-  weight=random(400,1500);
-
-  car=createSprite(50,200,50,50);
-  wall=createSprite(1500,200,60,height/2);
-  wall.shapeColor=color(80,80,80);
-  car.velocityX=speed;
-  
-}
-
-function draw() {
-  background("black");  
-  if(car.x - wall.x <( wall.width/2 + car.width/2)){
-      car.velocityX=0;
-      var deformation=0.5*weight*speed*speed/22500;
-      if(deformation>180){
-        car.shapeColor=color(255,0,0);
-      }
-      if(deformation<180 && deformation>100){
-        car.shapeColor=color(230,230,0);
-      }
-      if(deformation<100){
-        car.shapeColor=color(0,255,0);
-
-      }
-
-  }
-
-
-
-  drawSprites();
-}
+  //createSprite(400, 200, 50, 50);
+  fixedR = createSprite(400,200,80,50);
+  movingR = createSprite(600,200,50,100); 
+  movingR.shapeColor = "black"; 
+  movingR.debug = true; fixedR.debug = true; } 
+  function draw() { 
+    background("lightblue"); 
+    drawSprites(); 
+    movingR.y = World.mouseY; 
+    movingR.x = World.mouseX; 
+    //rectMode(CENTER); 
+    //rect (400,200,100,100); 
+    if(movingR.x - fixedR.x <= movingR.width/2 + fixedR.width/2 && 
+      fixedR.x - movingR.x <= movingR.width/2 + fixedR.width/2 &&
+      movingR.y - fixedR.y <= movingR.height/2 + fixedR.height/2 &&
+      fixedR.y - movingR.y <= movingR.height/2 + fixedR.height/2){ 
+        movingR.shapeColor = "green"; 
+        fixedR.shapeColor = "green"; } 
+    else{ movingR.shapeColor = "black"; 
+        fixedR.shapeColor = "grey"; } }
